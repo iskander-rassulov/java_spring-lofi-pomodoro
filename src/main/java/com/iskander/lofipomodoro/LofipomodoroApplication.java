de90fd5927e7b9532bcf890fd8deebe18ceb6cfc
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2024-10-05 01:17:01 +0500

    init commit

---
+++ src/main/java/com/iskander/lofipomodoro/LofipomodoroApplication.java
@@ -0,0 +1,13 @@
+package com.iskander.lofipomodoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LofipomodoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LofipomodoroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iskander/lofipomodoro/LofipomodoroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iskander.lofipomodoro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LofipomodoroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
