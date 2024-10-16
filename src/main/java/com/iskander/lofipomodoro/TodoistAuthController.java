d62038053e25ceeca170fe8ccdc19fa9ee588790
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2024-10-16 06:12:44 +0500

    todoist login v2

--- src/main/java/com/iskander/lofipomodoro/TodoistAuthController.java
+++ src/main/java/com/iskander/lofipomodoro/TodoistAuthController.java
@@ -1,36 +1,38 @@
 package com.iskander.lofipomodoro;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.net.URI;
+
+@RestController
+@RequestMapping("/todoist")
 public class TodoistAuthController {
 
     @Value("${TODOIST_CLIENT_ID}")
     private String clientId;
 
-    @Value("${TODOIST_CLIENT_SECRET}")
-    private String clientSecret;
+    @Value("${TODOIST_REDIRECT_URI}")
+    private String redirectUri;
 
     @GetMapping("/login")
-    public RedirectView login() {
-        String authUrl = "https://todoist.com/oauth/authorize" +
-                "?client_id=" + clientId +
-                "&scope=data:read_write" +
-                "&redirect_uri=http://localhost:8080/callback";
-        return new RedirectView(authUrl);
+    public ResponseEntity<Void> loginWithTodoist() {
+        String authorizationUrl = "https://todoist.com/oauth/authorize?client_id=" + clientId + "&scope=data:read&state=random_state_string&redirect_uri=" + redirectUri;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(URI.create(authorizationUrl));
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
     }
 
     @GetMapping("/callback")
-    public RedirectView callback(@RequestParam("code") String code) {
-        // Здесь обработка кода авторизации и получение токена
-        // Вы можете создать сервис для взаимодействия с API Todoist
-
-        // После получения токена, можно получить информацию о пользователе
-        return new RedirectView("/user-info");
+    public ResponseEntity<String> todoistCallback(@RequestParam("code") String code) {
+        // Здесь вы можете получить access token с использованием переданного code
+        // Логика получения токена будет включать отправку POST-запроса к API Todoist
+        return ResponseEntity.ok("Authorization successful! Code: " + code);
     }
 }
-
